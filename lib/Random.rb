041cfe26ae1aefa7d45ba11958d6a1362fc2d910
ashley williams <heyashleyashley@gmail.com>
2013-04-18 18:37:28 -0400

    add random image example

--- lib/Random.rb
+++ lib/Random.rb
@@ -2,7 +2,12 @@ class Random
 
   WORDS = ["hello","goodbye","poop","what","this","yuck"]
 
-  def self.getRandomColor()
+  def self.getRandomNumber()
+    nums = [1,2,3,4,5,6,7,8,9,10]
+    return nums.sample
+  end
+
+  def self.getRandomColor
     return '#' + "%06x" % (rand * 0xffffff)
   end
 
